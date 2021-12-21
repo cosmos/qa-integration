5726c2b3a873c699cddf833bc27b43dd4d9b0237
kaustubhkapatral <kaustubh.kapatral@gmail.com>
2021-12-21 12:21:18 +0530

    refactored scripts

---
+++ deps/env-check.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+display_usage() {
+    printf "** Please check the exported values:: **\n Daemon : $DAEMON\n Denom : $DENOM\n ChainID : $CHAINID\n DaemonHome : $DAEMON_HOME\n \n Github URL : $GH_URL\n Chain Version : $CHAIN_VERSION\n"
+    exit 1
+}
+
+if [ -z $DAEMON ] || [ -z $DENOM ] || [ -z $CHAINID ] || [ -z $DAEMON_HOME ] || [ -z $GH_URL ] || [ -z $CHAIN_VERSION ]
+then 
+    display_usage
+fi
