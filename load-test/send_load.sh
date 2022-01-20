9d0d3f228608c98f58f89ecf120d202e8ef67468
kaustubhkapatral <kaustubh.kapatral@gmail.com>
2022-01-20 17:34:55 +0530

    added support for multi msgs

--- load-test/send_load.sh
+++ load-test/send_load.sh
@@ -22,36 +22,54 @@ then
     PORT=16657
 fi
 
+cd ~/
 RPC="http://${IP}:${PORT}"
 acc1=$($DAEMON keys show account$FROM -a --home $DAEMON_HOME-1 --keyring-backend test)
 acc2=$($DAEMON keys show account$TO -a --home $DAEMON_HOME-1 --keyring-backend test)
-seq1=$("${DAEMON}" q account "${acc1}" --node $RPC --output json)
-seq2=$("${DAEMON}" q account "${acc2}" --node $RPC --output json)
-seq1no=$(echo "${seq1}" | jq -r '.sequence')
-seq2no=$(echo "${seq2}" | jq -r '.sequence')
 balance1=$("${DAEMON}" q bank balances "${acc1}" --node $RPC --output json)
 balance1res=$(echo "${balance1}" | jq -r '.balances')
 echo "** Balance of Account 1 before send_load :: $balance1res **"
 balance2=$("${DAEMON}" q bank balances "${acc2}" --node $RPC --output json)
-balance2res=$(echo "${balance1}" | jq -r '.balances')
+balance2res=$(echo "${balance2}" | jq -r '.balances')
 echo "** Balance of Account 2 before send_load :: $balance2res **"
-bound1=`expr 10000 + $seq1no`
-bound2=`expr 10000 + $seq2no`
-for (( a=$seq1no; a<$bound1; a++ ))
+for (( a=0; a<35; a++ ))
 do
-    sTx=$("${DAEMON}" tx bank send "${acc1}" "${acc2}" 1000000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-1 --node $RPC --output json -y --sequence $a &) 
-    sTxHash=$(echo "${sTx}")
+		unsignedTx=$("${DAEMON}" tx bank send "${acc1}" "${acc2}" 1000000"${DENOM}" --chain-id "${CHAINID}" --output json --generate-only --gas 500000 > unsigned.json)
+		unsignedTxres=$(echo "${unsignedTx}")
+		for (( b=0; b<30; b++))
+		do
+    		cat unsigned.json | jq '.body.messages |= . + [.[-1]]' > unsigned.json.bk && mv unsigned.json.bk unsigned.json 
+		done
+		seq1=$("${DAEMON}" q account "${acc1}" --node $RPC --output json)
+		seq1no=$(echo "${seq1}" | jq -r '.sequence')
+		signTx=$("${DAEMON}" tx sign unsigned.json --from "${acc1}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-1 --node $RPC --signature-only=false --sequence $seq1no --gas 500000 > signed.json)
+		signTxres=$(echo "${signTx}")
+		broadcast=$("${DAEMON}" tx broadcast signed.json --output json --chain-id "${CHAINID}" --gas 500000 --node $RPC --broadcast-mode async)
+		broadcastRes=$(echo "${broadcast}" | jq .txhash)
+		echo $broadcastRes
 done
 
-for (( b=$seq2no; b<$bound2; b++ ))
+for (( a=0; a<35; a++ ))
 do
-    sTx=$("${DAEMON}" tx bank send "${acc2}" "${acc1}" 1000000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-1 --node $RPC --output json -y --sequence $b &) 
-    sTxHash=$(echo "${sTx}")
+		unsignedTx=$("${DAEMON}" tx bank send "${acc2}" "${acc1}" 1000000"${DENOM}" --chain-id "${CHAINID}" --output json --generate-only --gas 500000 > unsigned.json)
+		unsignedTxres=$(echo "${unsignedTx}")
+		for (( b=0; b<30; b++))
+		do
+    		cat unsigned.json | jq '.body.messages |= . + [.[-1]]' > unsigned.json.bk && mv unsigned.json.bk unsigned.json 
+		done
+		seq2=$("${DAEMON}" q account "${acc2}" --node $RPC --output json)
+		seq2no=$(echo "${seq2}" | jq -r '.sequence')
+		signTx=$("${DAEMON}" tx sign unsigned.json --from "${acc2}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-1 --node $RPC --signature-only=false --sequence $seq1no --gas 500000 > signed.json)
+		signTxres=$(echo "${signTx}")
+		broadcast=$("${DAEMON}" tx broadcast signed.json --output json --chain-id "${CHAINID}" --gas 500000 --node $RPC --broadcast-mode async)
+		broadcastRes=$(echo "${broadcast}" | jq .txhash)
+		echo $broadcastRes
 done
 
+sleep 7s
 balance1=$("${DAEMON}" q bank balances "${acc1}" --node $RPC --output json)
 balance1res=$(echo "${balance1}" | jq -r '.balances')
 echo "** Balance of Account 1 after send_load :: $balance1res **"
 balance2=$("${DAEMON}" q bank balances "${acc2}" --node $RPC --output json)
-balance2res=$(echo "${balance1}" | jq -r '.balances')
+balance2res=$(echo "${balance2}" | jq -r '.balances')
 echo "** Balance of Account 2 after send_load :: $balance2res **"
