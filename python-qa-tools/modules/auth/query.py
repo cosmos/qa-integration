a313b109e8f285e61c915dce5c724957f90af86d
juggernaut09 <venkatteja.ravi@gmail.com>
2022-06-06 07:01:00 +0000

    Added docs for modules/auth/query.py

--- python-qa-tools/modules/auth/query.py
+++ python-qa-tools/modules/auth/query.py
@@ -5,14 +5,16 @@ from utils import exec_command
 DAEMON = os.getenv('DAEMON')
 RPC = os.getenv('RPC')
 
-# query account function will take the bech32 address as input and output the information of account.
 
+# account type is an user-defined data type used to validate account addresses read from Argument Parser.
 def account_type(address):
     status, response = query_account(address)
     if not status:
         raise argparse.ArgumentTypeError(response)
     return address
 
+
+# query account function will take the bech32 address as input and output the information of account.
 def query_account(address):
     try:
         command = f"{DAEMON} query auth account {address} --node {RPC} --output json"
@@ -22,3 +24,4 @@ def query_account(address):
         return True, json.loads(stdout)
     except Exception as e:
         return False, e
+    
