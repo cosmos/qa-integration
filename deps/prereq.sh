0840f04045293743c8d4f9f0ddabf3499455ef59
akhilkumarpilli <akhilkumar7947@gmail.com>
2022-06-08 11:21:35 +0530

    Modify export steps

--- deps/prereq.sh
+++ deps/prereq.sh
@@ -22,19 +22,19 @@ else
   tar -xvf go$goversion.linux-amd64.tar.gz
   sudo mv go /usr/local
   rm go$goversion.linux-amd64.tar.gz
+  export GOPATH=$HOME/go
+  echo "" >> ~/.bashrc
+  echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 fi
 
 if [[ -z $GOPATH || -z $GOBIN || "$PATH" != *"$SGOBIN"* ]]; then
   echo "------ Update bashrc ---------------"
   export GOPATH=$HOME/go
-  export GOROOT=/usr/local/go
   export GOBIN=$GOPATH/bin
-  export PATH=$PATH:/usr/local/go/bin:$GOBIN
   echo "" >> ~/.bashrc
   echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-  echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
   echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
-  echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOROOT/bin:$GOBIN' >> ~/.bashrc
 fi
 source ~/.bashrc
 mkdir -p "$GOBIN"
