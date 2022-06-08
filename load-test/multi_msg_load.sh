e7d4064b9c09b2f51a62d79331468a7045f39452
akhilkumarpilli <akhilkumar7947@gmail.com>
2022-06-08 11:31:22 +0530

    Debug pythonpath

--- load-test/multi_msg_load.sh
+++ load-test/multi_msg_load.sh
@@ -9,6 +9,8 @@ cd $CURPATH
 # check environment variables are set
 . ../deps/env-check.sh
 
+echo $PYTHONPATH
+
 # we can pass optional arguments when running this script
 # available arguments are -s/--sender, -r/--receiver, -n/--num_txs, -h/--help
 # example: ./multi_msg_load.sh -s cosmos1f2838advrjl3c8h4kjfvfmhkh0gs0wf6cyzwu8 -r osmo1cytlejwrejz8wajslgqwczlzazxaxhf4hccly5 -n 100
