09f2ac783f6723eba1e5603036569c70168ebd83
akhilkumarpilli <akhilkumar7947@gmail.com>
2022-06-10 16:20:29 +0530

    feat: implement tx summary counter

--- python-qa-tools/load-test/multi_msg_load.py
+++ python-qa-tools/load-test/multi_msg_load.py
@@ -4,35 +4,67 @@ from core.keys import keys_show
 from modules.auth.query import account_type, query_account
 from modules.bank.query import query_balances
 from modules.bank.tx import sign_and_broadcast_txs, create_unsigned_txs
-from utils import create_multi_messages, validate_num_txs, print_balance_deductions
+from utils import (
+    create_multi_messages,
+    validate_num_txs,
+    print_balance_deductions,
+    check_tx_result,
+    print_tx_summary,
+)
 
-HOME = os.getenv('HOME')
-NUM_MSGS = int(os.getenv('NUM_MSGS'))
- 
-logging.basicConfig(format='%(message)s',
-                    level=logging.DEBUG)
+HOME = os.getenv("HOME")
+NUM_MSGS = int(os.getenv("NUM_MSGS"))
 
-parser = argparse.ArgumentParser(description='This program takes inputs for intializing multi message load test.')
-parser.add_argument('-s', '--sender', type = account_type, default = keys_show("account1")[1]['address'], help = 'Sender bech32 address')
-parser.add_argument('-r', '--receiver', type= account_type, default = keys_show("account2")[1]['address'], help= 'Receiver bech32 address')
-parser.add_argument('-n', '--num_txs', type = validate_num_txs, default = 1000, help= 'Number of transactions to be made, should be positive integer')
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(
+    description="This program takes inputs for intializing multi message load test."
+)
+parser.add_argument(
+    "-s",
+    "--sender",
+    type=account_type,
+    default=keys_show("account1")[1]["address"],
+    help="Sender bech32 address",
+)
+parser.add_argument(
+    "-r",
+    "--receiver",
+    type=account_type,
+    default=keys_show("account2")[1]["address"],
+    help="Receiver bech32 address",
+)
+parser.add_argument(
+    "-n",
+    "--num_txs",
+    type=validate_num_txs,
+    default=1000,
+    help="Number of transactions to be made, should be positive integer",
+)
 args = parser.parse_args()
-sender, receiver, NUM_TXS, amount_to_be_sent = args.sender, args.receiver, int(args.num_txs), 1000000
+sender, receiver, NUM_TXS, amount_to_be_sent = (
+    args.sender,
+    args.receiver,
+    int(args.num_txs),
+    1000000,
+)
 
 if sender == receiver:
-    sys.exit('Error: The values of arguments "sender" and "receiver" are equal make sure to set different values')
+    sys.exit(
+        'Error: The values of arguments "sender" and "receiver" are equal make sure to set different values'
+    )
 
 # Fetch balances of sender and receiver accounts before executing the load test
-status, sender_balance_old= query_balances(sender)
+status, sender_balance_old = query_balances(sender)
 if not status:
     sys.exit(sender_balance_old)
-sender_balance_old = int(sender_balance_old['balances'][0]['amount'])
+sender_balance_old = int(sender_balance_old["balances"][0]["amount"])
 
 # Fetch Balances of receiver executing the load test
 status, receiver_balance_old = query_balances(receiver)
 if not status:
     sys.exit(receiver_balance_old)
-receiver_balance_old = int(receiver_balance_old['balances'][0]['amount'])
+receiver_balance_old = int(receiver_balance_old["balances"][0]["amount"])
 
 # Fetching sequence numbers of to and from accounts
 status, sender_acc = query_account(sender)
@@ -43,55 +75,96 @@ status, receiver_acc = query_account(receiver)
 if not status:
     sys.exit(receiver_acc)
 
-sender_acc_seq, receiver_acc_seq = int(sender_acc['sequence']), int(receiver_acc['sequence'])    
+sender_acc_seq, receiver_acc_seq = int(sender_acc["sequence"]), int(
+    receiver_acc["sequence"]
+)
 
-# Generating unsigned transactions with a single transfer message 
-status, unsignedTxto = create_unsigned_txs(sender, receiver, amount_to_be_sent, 'unsignedto.json')
+# Generating unsigned transactions with a single transfer message
+status, unsignedTxto = create_unsigned_txs(
+    sender, receiver, amount_to_be_sent, "unsignedto.json"
+)
 if not status:
     logging.error(unsignedTxto)
 
-status, unsignedTxfrom = create_unsigned_txs(receiver, sender, amount_to_be_sent, 'unsignedfrom.json')
+status, unsignedTxfrom = create_unsigned_txs(
+    receiver, sender, amount_to_be_sent, "unsignedfrom.json"
+)
 if not status:
     logging.error(unsignedTxfrom)
-        
+
+num_success_txs, num_failed_txs, num_other_errors, failed_code_errors = 0, 0, 0, {}
+
 for i in range(NUM_TXS):
-        
-    # Duplicating and appending transfer message in the existing array to create a multi-msg transaction        
-    create_multi_messages(NUM_MSGS, 'unsignedto.json')
-    create_multi_messages(NUM_MSGS, 'unsignedfrom.json')
+
+    # Duplicating and appending transfer message in the existing array to create a multi-msg transaction
+    create_multi_messages(NUM_MSGS, "unsignedto.json")
+    create_multi_messages(NUM_MSGS, "unsignedfrom.json")
 
     # Signing and broadcasting the unsigned transactions from sender to receiver
     seqto = sender_acc_seq + i
-    status, txHash = sign_and_broadcast_txs('unsignedto.json', 'signedto.json', sender, seqto)
-    if not status: # if the txn is successful
-        logging.error(f"sign_and_broadcast_tx from sender to receiver failed : {txHash}")
-    else:
-        logging.info(f"broadcasted txhash: {txHash}")
+    logging.info(f"Signing and broadcasting tx: {i*2+1}")
+    status, tx = sign_and_broadcast_txs(
+        "unsignedto.json", "signedto.json", sender, seqto
+    )
+    (
+        failed_code_errors,
+        num_success_txs,
+        num_failed_txs,
+        num_other_errors,
+    ) = check_tx_result(
+        tx,
+        status,
+        failed_code_errors,
+        num_success_txs,
+        num_failed_txs,
+        num_other_errors,
+    )
 
     # Signing and broadcasting the unsigned transactions from receiver to sender
     seqfrom = receiver_acc_seq + i
-    status, txHash = sign_and_broadcast_txs('unsignedfrom.json', 'signedfrom.json', receiver, seqfrom)
-    if not status: # if the txn is unsuccessful
-        logging.error(f"sign_and_broadcast_tx from receiver to sender failed : {txHash}")
-    else:
-        logging.info(f"broadcasted txhash: {txHash}")
+    logging.info(f"Signing and broadcasting tx: {i*2+2}")
+    status, tx = sign_and_broadcast_txs(
+        "unsignedfrom.json", "signedfrom.json", receiver, seqfrom
+    )
+    (
+        failed_code_errors,
+        num_success_txs,
+        num_failed_txs,
+        num_other_errors,
+    ) = check_tx_result(
+        tx,
+        status,
+        failed_code_errors,
+        num_success_txs,
+        num_failed_txs,
+        num_other_errors,
+    )
 
-logging.info('waiting for tx confirmation, avg time is 7s.')
+logging.info("waiting for tx confirmation, avg time is 7s.")
 time.sleep(7)
 
 # Verifying the balance deductions
 status, sender_balance_updated = query_balances(sender)
 if not status:
     sys.exit(sender_balance_updated)
-sender_balance_updated = sender_balance_updated['balances'][0]['amount']
+sender_balance_updated = sender_balance_updated["balances"][0]["amount"]
 
 status, receiver_balance_updated = query_balances(receiver)
 if not status:
     sys.exit(receiver_balance_updated)
-receiver_balance_updated = receiver_balance_updated['balances'][0]['amount']
+receiver_balance_updated = receiver_balance_updated["balances"][0]["amount"]
 
 sender_diff = int(sender_balance_old) - int(sender_balance_updated)
 receiver_diff = int(receiver_balance_old) - int(receiver_balance_updated)
 
-print_balance_deductions('sender', sender_diff)
-print_balance_deductions('receiver', receiver_diff)
+print_balance_deductions("sender", sender_diff)
+print_balance_deductions("receiver", receiver_diff)
+
+print_tx_summary(
+    NUM_TXS * 2,
+    NUM_TXS * 2 * NUM_MSGS,
+    failed_code_errors,
+    num_success_txs,
+    num_failed_txs,
+    num_other_errors,
+)
--- python-qa-tools/utils.py
+++ python-qa-tools/utils.py
@@ -1,61 +1,131 @@
 import argparse, os, json, logging, subprocess
 
-logging.basicConfig(format='%(message)s',
-                    level=logging.DEBUG)
+from black import err
 
-DAEMON = os.getenv('DAEMON')
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
-HOME = os.getenv('HOME')
+DAEMON = os.getenv("DAEMON")
+
+HOME = os.getenv("HOME")
 
 # The function `print_balance_deductions` will print information about the balance deductions after transactions for a wallet or account.
 def print_balance_deductions(wallet, diff):
     if diff > 0:
-        logging.error('Some of the transactions failed')
+        logging.error("Some of the transactions failed")
         logging.info(f"Balance in the {wallet} increased by {diff}")
     elif diff < 0:
-        logging.error('Some of the transactions failed')
+        logging.error("Some of the transactions failed")
         logging.info(f"Balance in the {wallet} decreased by {-1 * diff}")
     else:
-        logging.info(f"All transaction went successfully, No deduction from {wallet} balance")
-        
+        logging.info(
+            f"All transaction went successfully, No deduction from {wallet} balance"
+        )
+
 
 # The utility function `exec_command` is used to execute the cosmos-sdk based commands.
 def exec_command(command):
-    stdout, stderr = subprocess.Popen(command.split(),
-                                    stdout = subprocess.PIPE,
-                                    stderr = subprocess.PIPE).communicate()
+    stdout, stderr = subprocess.Popen(
+        command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ).communicate()
     return stdout.strip().decode(), stderr.strip().decode()
+
 
 # The utility function `is_tool` is used to verify the package or binary installation.
 def is_tool(binary):
     """Check whether `name` is on PATH and marked as executable."""
     from shutil import which
+
     return which(binary) is not None
+
 
 # validate_num_txs will validate num_txs value
 def validate_num_txs(x):
     if int(x) < 1:
-        raise argparse.ArgumentTypeError('The argument NUM_TXS should be positive integer')
+        raise argparse.ArgumentTypeError(
+            "The argument NUM_TXS should be positive integer"
+        )
     return int(x)
+
 
 # node_type is a user-defined type.
 def node_type(x):
     x = int(x)
     if x < 2:
-        raise argparse.ArgumentTypeError(f"The number of nodes should be atleast 2, you have entered {x}")
+        raise argparse.ArgumentTypeError(
+            f"The number of nodes should be atleast 2, you have entered {x}"
+        )
     return x
+
 
 # The function `create_multi_messages` is used to duplicate the messages in a single transaction.
 def create_multi_messages(num_msgs, file_name):
     messages = []
-    with open(f"{HOME}/{file_name}", 'r+') as file:
+    with open(f"{HOME}/{file_name}", "r+") as file:
         file_data = json.load(file)
         messages.append(file_data["body"]["messages"][-1])
     for i in range(num_msgs):
         messages.append(messages[-1])
-        
-    with open(f"{HOME}/{file_name}", 'r+') as file:
+
+    with open(f"{HOME}/{file_name}", "r+") as file:
         file_data = json.load(file)
         file_data["body"]["messages"] = messages
         file.seek(0)
-        json.dump(file_data, file, indent = 4)
+        json.dump(file_data, file, indent=4)
+
+
+# check_tx_result checks tx whether failed or success and update count based on it
+def check_tx_result(
+    tx_result,
+    status,
+    failed_code_errors,
+    num_success_txs,
+    num_failed_txs,
+    num_other_errors,
+):
+    if not status:
+        num_failed_txs += 1
+        logging.error(f"ERROR: {tx_result}")
+        if type(tx_result) is dict and "code" in tx_result:
+            if tx_result["code"] not in failed_code_errors:
+                failed_code_errors[tx_result["code"]] = []
+            error_type = tx_result["raw_log"]
+            split_raw_log = error_type.split(":")
+            if len(split_raw_log):
+                error_type = split_raw_log[-1].strip()
+
+            failed_code_errors[tx_result["code"]].append(error_type)
+        else:
+            num_other_errors += 1
+
+    else:
+        num_success_txs += 1
+
+    return failed_code_errors, num_success_txs, num_failed_txs, num_other_errors
+
+
+def print_tx_summary(
+    num_txs,
+    num_msgs,
+    failed_code_errors,
+    num_success_txs,
+    num_failed_txs,
+    num_other_errors,
+):
+    logging.info(
+        f"""
+Testing Stats:
+-----------------------------
+Number of transactions executed: {num_txs}
+Number of messages executed: {num_msgs}
+Number of successful transactions: {num_success_txs} ({(num_success_txs/num_txs)*100}%)
+Number of failed transactions: {num_failed_txs} ({(num_failed_txs/num_txs)*100}%)
+        """
+    )
+    if num_failed_txs:
+        logging.info("Failures:")
+        for key, value in failed_code_errors.items():
+            logging.info(f"Failed with code {key} ({value[0]}): {len(value)}")
+        if num_other_errors:
+            logging.info(f"Other errors: {num_other_errors}")
+
+    logging.info("-----------------------------")
