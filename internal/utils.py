93031e33e836370dda5210695aa1066830c25241
juggernaut09 <venkatteja.ravi@gmail.com>
2022-06-22 10:08:09 +0000

    Stages utils.py

--- internal/utils.py
+++ internal/utils.py
@@ -1,88 +1,129 @@
-import argparse, os, json, logging, subprocess
-
-from stats import record_stat, TX_TYPE, QUERY_TYPE
+"""
+This module contains all util functions.
+"""
+import argparse
+import os
+import json
+import logging
+import subprocess
+from shutil import which
 
 logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
 DAEMON = os.getenv("DAEMON")
-
 HOME = os.getenv("HOME")
 
-# The function `print_balance_deductions` will print information about the balance deductions after transactions for a wallet or account.
+
 def print_balance_deductions(wallet, diff):
+    """
+    The function `print_balance_deductions` will print information about the
+    balance deductions after transactions for a wallet or account.
+    Args:
+        wallet (_str_): wallet name
+        diff (_uint_): balance difference.
+    """
     if diff > 0:
         logging.error("Some of the transactions failed")
-        logging.info(f"Balance in the {wallet} increased by {diff}")
+        logging.info("Balance in the %s increased by %s", wallet, diff)
     elif diff < 0:
         logging.error("Some of the transactions failed")
-        logging.info(f"Balance in the {wallet} decreased by {-1 * diff}")
+        logging.info("Balance in the %s decreased by %s", wallet, (-1 * diff))
     else:
         logging.info(
-            f"All transaction went successfully, No deduction from {wallet} balance"
+            "All transaction went successfully, No deduction from %s balance", wallet
         )
 
 
-# The utility function `exec_command` is used to execute the cosmos-sdk based commands.
 def exec_command(command):
-    try:
-        test_type = os.getenv("TEST_TYPE") if os.getenv("TEST_TYPE") else None
-        # getting command type
-        sub_commands = command.split()
-        cmd_type = None
-        if len(sub_commands) > 1 and (
-            sub_commands[1] == "q" or sub_commands[1] == "query"
-        ):
-            cmd_type = QUERY_TYPE
-        elif len(sub_commands) > 1 and (sub_commands[1] == "tx"):
-            cmd_type = TX_TYPE
+    """
+    The utility function `exec_command` is used to execute the cosmos-sdk based commands.
+    Args:
+        command (_str_): Command to be executed.
 
-        stdout, stderr = subprocess.Popen(
-            command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        ).communicate()
-        out, err = stdout.strip().decode(), stderr.strip().decode()
-        if test_type and cmd_type:
-            record_stat(test_type, cmd_type, out, err)
-        return out, err
-    except Exception as e:
-        if test_type and cmd_type:
-            record_stat(test_type, cmd_type, "", e)
-        return None, e
+    Returns:
+        _tuple_: str, str
+    """
+    std_out, std_err = subprocess.Popen(
+        command.split(), std_out=subprocess.PIPE, std_err=subprocess.PIPE
+    ).communicate()
+    return std_out.strip().decode(), std_err.strip().decode()
 
 
-# The utility function `is_tool` is used to verify the package or binary installation.
 def is_tool(binary):
-    """Check whether `name` is on PATH and marked as executable."""
-    from shutil import which
-
+    """
+    The utility function `is_tool` is used to verify the package or binary installation.
+    """
     return which(binary) is not None
 
 
-# validate_num_txs will validate num_txs value
-def validate_num_txs(x):
-    if int(x) < 1:
+def validate_num_txs(num_x):
+    """
+    validate_num_txs will validate num_txs value
+    Args:
+        num_x (_int_)
+
+    Raises:
+        argparse.ArgumentTypeError
+
+    Returns:
+        _int_: _int_
+    """
+    if int(num_x) < 1:
         raise argparse.ArgumentTypeError(
             "The argument NUM_TXS should be positive integer"
         )
-    return int(x)
+    return int(num_x)
 
 
-# node_type is a user-defined type.
-def node_type(x):
-    x = int(x)
-    if x < 2:
+def num_txs_type(num_x):
+    """
+    num_txs_type is a user-defined type.
+    Args:
+        num_x (_unit_): int
+
+    Raises:
+        argparse.ArgumentTypeError
+
+    Returns:
+        _int_: int
+    """
+    if int(num_x) < 1000:
+        raise argparse.ArgumentTypeError("The argument NUM_TXS should be 1000 or more")
+    return int(num_x)
+
+
+def node_type(num_x):
+    """
+    node_type is a user-defined type.
+    Args:
+        num_x (_int_): int
+
+    Raises:
+        argparse.ArgumentTypeError
+
+    Returns:
+        _int_: _int_
+    """
+    num_x = int(num_x)
+    if num_x < 2:
         raise argparse.ArgumentTypeError(
-            f"The number of nodes should be atleast 2, you have entered {x}"
+            f"The number of nodes should be atleast 2, you have entered {num_x}"
         )
-    return x
+    return num_x
 
 
-# The function `create_multi_messages` is used to duplicate the messages in a single transaction.
 def create_multi_messages(num_msgs, file_name):
+    """
+    The function `create_multi_messages` is used to duplicate the messages in a single transaction.
+    Args:
+        num_msgs (_uint_): uint
+        file_name (_str_): file path to modify messages.
+    """
     messages = []
     with open(f"{HOME}/{file_name}", "r+") as file:
         file_data = json.load(file)
         messages.append(file_data["body"]["messages"][-1])
-    for i in range(num_msgs):
+    for _i in range(num_msgs):
         messages.append(messages[-1])
 
     with open(f"{HOME}/{file_name}", "r+") as file:
