8e98a60f00a0f1f53be5a19c29e2d7d64f7209f2
PrathyushaLakkireddy <prathyusha@vitwit.com>
2022-06-27 11:26:31 +0530

    add tests for send tx

--- internal/modules/bank/test.py
+++ internal/modules/bank/test.py
@@ -1,6 +1,5 @@
-import os, sys, time, logging
+import time, logging
 from core.keys import keys_show
-from internal.utils import DAEMON, exec_command
 from modules.bank.tx import (
     tx_send,
 )
@@ -8,31 +7,53 @@ from modules.bank.query import (
     query_balances,
 )
 
-HOME = os.getenv("HOME")
-DAEMON = os.getenv("DAEMON")
 logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
-# get validator, delegator and dst validator address
-#validator = keys_show("validator1", "val", 1)[1]["address"]
-from_address = keys_show("account1", "acc", 1)[1]["address"]
-to_address = keys_show("account2", "acc", 1)[1]["address"]
-#dst_val_address = keys_show("validator2", "val", 2)[1]["address"]
+sender = keys_show("account1")[1]["address"]
+receiver = keys_show("account2")[1]["address"]
 
 # assign the arguments
 amount_to_be_sent = 5
 
-if from_address == to_address:
-    sys.exit(
-        'Error: The values of arguments "sender" and "receiver" are equal make sure to set different values'
-    )
+if sender == receiver:
+    logging.error(f"Error: The values of arguments sender and receiver are equal make sure to set different values")
+
+# Fetch balances of sender and receiver accounts before executing the send_tx
+status, sender_balance_old = query_balances(sender)
+if not status:
+    logging.error(sender_balance_old)
+sender_balance_old = int(sender_balance_old["balances"][0]["amount"])
+
+# Fetch balances of receiver
+status, receiver_balance_old = query_balances(receiver)
+if not status:
+    logging.error(receiver_balance_old)
+receiver_balance_old = int(receiver_balance_old["balances"][0]["amount"])
 
 # send tx
-status, send_tx = tx_send(from_address, to_address, amount_to_be_sent)
+status, send_tx = tx_send(sender, receiver, amount_to_be_sent)
 if not status:
-    logging.error(f"error in send tx :: {send_tx}")
+    logging.error(f"send tx status:: {status}")
 else:
     logging.info(f"tx_hash of send :: {send_tx['txhash']}")
 
 time.sleep(3)
 
-print(f"send tx......{send_tx}")
+# Fetch new balances of sender and receiver accounts after executing send_tx
+status, sender_balance_new = query_balances(sender)
+if not status:
+    logging.error(sender_balance_new)
+sender_balance_new = int(sender_balance_new["balances"][0]["amount"])
+
+# Fetch balances of receiver
+status, receiver_balance_new = query_balances(receiver)
+if not status:
+    logging.error(receiver_balance_new)
+receiver_balance_new = int(receiver_balance_new["balances"][0]["amount"])
+
+if ((sender_balance_old - amount_to_be_sent) == sender_balance_new) & (
+    (receiver_balance_old + amount_to_be_sent) == receiver_balance_new
+):
+    logging.info(f"send tx is successfull!!")
+else:
+    logging.error(f"send tx failed!!")
