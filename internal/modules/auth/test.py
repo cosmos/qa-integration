bb649cb6155723358092018152ca0a7495b99300
Anil Kumar Kammari <anil@vitwit.com>
2022-07-13 11:36:26 +0530

    add tests for x/auth query (#49)
    * add tests for auth query
    
    * refactor
    
    * try
    
    * update fake address
    
    * update make def
    
    * verify fake test
    
    * verified fake test.
    
    * add more test
    
    * Fix test
    
    * add logs
    
    * add separate GA for all modules
    
    * disable all other tests for testing
    
    * fix status
    
    * re-enable GA

---
+++ internal/modules/auth/test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from core.keys import keys_show
+from modules.auth.query import (
+    query_account,
+)
+
+class TestAuthModuleTxsQueries(unittest.TestCase):
+
+    def test_query_account(self):
+        status, key = keys_show("account1", "acc")
+        self.assertTrue(status)
+        self.assertTrue(key)
+        status, response = query_account(key["address"])
+        self.assertTrue(status)
+
+    def test_query_account_fail(self):
+        status, response = query_account("cosmos1xpcfd7pxfmv6gd50y9mwjq50kwqpqrh5mmw72h")
+        self.assertFalse(status)
+
+if __name__ == '__main__':
+    unittest.main()
