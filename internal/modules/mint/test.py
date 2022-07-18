349f0c7da35814aadc5c9d366f256439ce4204f8
NagaTulasi <tulasitulip@gmail.com>
2022-07-18 16:25:12 +0530

    upadte tests

--- internal/modules/mint/test.py
+++ internal/modules/mint/test.py
@@ -1,4 +1,4 @@
-import logging,time,json
+import logging,time,json,unittest
 from core.keys import keys_show
 from modules.mint.query import *
 
@@ -9,38 +9,28 @@ DAEMON_HOME = os.getenv("DAEMON_HOME")
 
 logging.info("INFO :: Running mint module test scripts")
 
+class TestMintModuleQueries(unittest.TestCase):
 
-def test_params_query():
-     # query params
-    path = f"{HOME}/.simd-1/config/"
-    with open(path+'genesis.json') as file:
-        data = json.load(file)
+    def test_params_query(self):
+         # query params
+        path = f"{HOME}/.simd-1/config/"
+        with open(path+'genesis.json') as file:
+            data = json.load(file)
 
-    query_param = query_params()[1]
+        query_param = query_params()[1]
 
-    if data["app_state"]["mint"]["params"] == query_param:
-        logging.info("querying params was successful")
-    else:
-        logging.error("missmatch in params")
-    time.sleep(5)
+        assert (data["app_state"]["mint"]["params"] == query_param), f"missmatch in params"
+        time.sleep(3)
 
-def test_inflation_query():
-    inflation = query_inflation()[1]
-    if inflation:
-        logging.info("Querying minting inflation successful")
-    else:
-        logging.info("Querying minting inflation was failed")
-    time.sleep(5)
+    def test_inflation_query(self):
+        status,inflation = query_inflation()
+        assert status, f"Querying minting inflation was failed"
+        time.sleep(3)
 
-def test_annual_provision_query():
-    provision = query_annual_provision()[1]
-    if provision:
-        logging.info("Querying minting annual provision successful")
-    else:
-        logging.info("Querying minting annual provision was failed")
-    time.sleep(5)
+    def test_annual_provision_query(self):
+        status,provision = query_annual_provision()
+        assert status, f"Querying minting annual provision was failed"
+        time.sleep(3)
 
-
-test_params_query()
-test_inflation_query()
-test_annual_provision_query()
+if __name__ == '__main__':
+    unittest.main()
