2ad959113ea0e6bd4d6cdc31926fe47802f73d13
PrathyushaLakkireddy <prathyusha@vitwit.com>
2022-07-18 11:01:40 +0530

    update tests

--- internal/modules/staking/test.py
+++ internal/modules/staking/test.py
@@ -2,6 +2,7 @@ import os, sys, time, logging
 from core.keys import keys_show
 from internal.utils import DAEMON, exec_command
 import tempfile
+import unittest
 from modules.staking.tx import (
     tx_delegate,
     tx_redelegate,
@@ -33,111 +34,100 @@ if sender == validator:
         'Error: The values of arguments "sender" and "receiver" are equal make sure to set different values'
     )
 
-# delegate tx
-def delegate_tx():
-    status, delegate_tx = tx_delegate("account1", validator, amount_to_be_sent)
-    assert status, f"error in delegation tx :: {delegate_tx}"
-    time.sleep(3)
 
-    # query delegation amount
-    before_del_amount = query_delegator_delegations(delegator, validator)[1]["balance"][
-        "amount"
-    ]
+class TestStakingModuleTxsQueries(unittest.TestCase):
 
-    status, delegateTx = tx_delegate("account1", validator, amount_to_be_sent)
-    assert status, f"error in delegation tx :: {delegate_tx}"
-    time.sleep(3)
+    # delegate tx
+    def test_delegate_tx(self):
+        status, delegate_tx = tx_delegate("account1", validator, amount_to_be_sent)
+        self.assertTrue(status)
+        time.sleep(3)
 
-    # query delegations and match the delagted amount
-    after_del_amount = query_delegator_delegations(delegator, validator)[1]["balance"][
-        "amount"
-    ]
-    if (int(before_del_amount) + amount_to_be_sent) == int(after_del_amount):
-        logging.info(f"delegation tx was successfull from {delegator} to {validator}")
-    else:
-        logging.error("mismatch in delegation amount!!")
+        # query delegation amount
+        before_del_amount = query_delegator_delegations(delegator, validator)[1][
+            "balance"
+        ]["amount"]
 
+        status, delegateTx = tx_delegate("account1", validator, amount_to_be_sent)
+        self.assertTrue(status)
+        time.sleep(3)
 
-# redelegation tx
-def redelegate_tx():
-    status, delegate_tx = tx_delegate("account1", dst_val_address, amount_to_be_sent)
-    assert status, f"delegation tx status:: {status}"
-    time.sleep(3)
-
-    # query redelegated amount
-    before_redel_amount = query_delegator_delegations(delegator, dst_val_address)[1][
-        "balance"
-    ]["amount"]
+        # query delegations and match the delagted amount
+        after_del_amount = query_delegator_delegations(delegator, validator)[1][
+            "balance"
+        ]["amount"]
+        assert (int(before_del_amount) + amount_to_be_sent) == int(
+            after_del_amount
+        ), f"mismatch in delegation amount!!!"
 
     # redelegation tx
-    status, redelegate_tx = tx_redelegate(
-        "account1", validator, dst_val_address, amount_to_be_sent
-    )
-    assert status, f"redelegation tx status :: {status}"
-    time.sleep(3)
-
-    # query redelegated amount and match the amount before and after the tx
-    after_redel_amount = query_delegator_delegations(delegator, dst_val_address)[1][
-        "balance"
-    ]["amount"]
-    if (int(before_redel_amount) + amount_to_be_sent) == int(after_redel_amount):
-        logging.info(
-            f"redelegation tx was successfull from {delegator} to {dst_val_address}"
+    def test_redelegate_tx(self):
+        status, delegate_tx = tx_delegate(
+            "account1", dst_val_address, amount_to_be_sent
         )
-    else:
-        logging.error("tx failed! mismatch in redelegation amount.")
+        self.assertTrue(status)
+        time.sleep(3)
 
+        # query redelegated amount
+        before_redel_amount = query_delegator_delegations(delegator, dst_val_address)[
+            1
+        ]["balance"]["amount"]
 
-# unbond tx
-def unbond_tx():
-    status, unbond_tx = tx_unbond("account1", validator, amount_to_be_sent)
-    assert status, f"unbond tx status :: {status}"
-    time.sleep(3)
+        # redelegation tx
+        status, redelegate_tx = tx_redelegate(
+            "account1", validator, dst_val_address, amount_to_be_sent
+        )
+        self.assertTrue(status)
+        time.sleep(3)
 
-    # query unbond tx and check the unbonded amount
-    status, unbond_amount = query_unbonding_delegation(delegator, validator)
-    unbond_balance = unbond_amount["entries"][0]["balance"]
-    if amount_to_be_sent == int(unbond_balance):
-        logging.info(f"unbond tx is successfull!!!")
-    else:
-        logging.error("error in unbond tx!!!")
+        # query redelegated amount and match the amount before and after the tx
+        after_redel_amount = query_delegator_delegations(delegator, dst_val_address)[1][
+            "balance"
+        ]["amount"]
+        assert (int(before_redel_amount) + amount_to_be_sent) == int(
+            after_redel_amount
+        ), f"tx failed! mismatch in redelegation amount!!!"
 
+    # unbond tx
+    def test_unbond_tx(self):
+        status, unbond_tx = tx_unbond("account1", validator, amount_to_be_sent)
+        self.assertTrue(status)
+        time.sleep(3)
 
-# create validator
-def create_validator():
-    temp_dir = tempfile.TemporaryDirectory()
-    temp_dir_name = temp_dir.name
-    TEMP_VAL = "validator-10000"
+        # query unbond tx and check the unbonded amount
+        status, unbond_amount = query_unbonding_delegation(delegator, validator)
+        unbond_balance = unbond_amount["entries"][0]["balance"]
+        assert amount_to_be_sent == int(unbond_balance), f"error in unbond tx!!!"
 
-    command = f"{DAEMON} init testvalidator --home {temp_dir_name}"
-    tx, tx_err = exec_command(command)
-    assert len(tx_err), f"init tx res :: {tx}"
+    # create validator
+    def test_create_validator(self):
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_dir_name = temp_dir.name
+        TEMP_VAL = "validator-10000"
 
-    status, create_val_tx = tx_create_validator(
-        "account1", amount_to_be_sent, TEMP_VAL, temp_dir_name
-    )
-    assert status, f"create-validator tx status :: {status}"
-    time.sleep(3)
+        command = f"{DAEMON} init testvalidator --home {temp_dir_name}"
+        tx, tx_err = exec_command(command)
+        assert len(tx_err), f"init tx res :: {tx}"
 
-    (_, validator) = keys_show("account1", "val")
+        status, create_val_tx = tx_create_validator(
+            "account1", amount_to_be_sent, TEMP_VAL, temp_dir_name
+        )
+        self.assertTrue(status)
+        time.sleep(3)
 
-    # query unbond tx and check the unbonded amount
-    status, validator_tx = query_validator(validator["address"])
-    if not status:
-        logging.error(f"query validator status :: {status}")
-    else:
+        (_, validator) = keys_show("account1", "val")
+
+        # query unbond tx and check the unbonded amount
+        status, validator_tx = query_validator(validator["address"])
+        self.assertTrue(status)
         val_name = validator_tx["description"]["moniker"]
-        if val_name == TEMP_VAL:
-            logging.info("validator created successfully!!!")
-        else:
-            logging.error(f"no validator found with name {TEMP_VAL}")
+        assert val_name == TEMP_VAL, f"no validator found with name {TEMP_VAL}"
 
-    # clean tmp dir
-    temp_dir.cleanup()
+        # clean tmp dir
+        temp_dir.cleanup()
 
 
-#call tests
-delegate_tx()
-redelegate_tx()
-unbond_tx()
-create_validator()
+if __name__ == "__main__":
+    logging.info("INFO: running staking module tests")
+    unittest.main()
+    logging.info("PASS: all staking module tests")
