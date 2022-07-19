9de32410881462495d08a8451d4c424696f5d739
PrathyushaLakkireddy <prathyusha@vitwit.com>
2022-07-19 17:28:14 +0530

    update comments

--- internal/modules/feegrant/query.py
+++ internal/modules/feegrant/query.py
@@ -4,13 +4,13 @@ DAEMON = env.DAEMON
 RPC = env.RPC
 CHAINID = env.CHAINID
 
-# `query_feegrant_grant` takes grannter and grantee addresses as pramaters and returns json response.
+# `query_feegrant_grant` queries details of a single grant.
 def query_feegrant_grant(granter, grantee):
     command = f"{DAEMON} q feegrant grant {granter} {grantee} --node {RPC} --chain-id {CHAINID} --output json"
     return exec_command(command)
 
 
-# `query_greantee_grants` takes grantee address as paramter and returns the grants of grantee in json response.
+# `query_greantee_grants` queries all grants of a grantee
 def query_greantee_grants(grantee):
     command = f"{DAEMON} q feegrant grants {grantee} --node {RPC} --chain-id {CHAINID} --output json --count-total"
     return exec_command(command)
