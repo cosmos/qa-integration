4aeb508e7a54a93ff7f1ef60a1a48ea72007c67c
NagaTulasi <tulasitulip@gmail.com>
2022-07-20 16:16:23 +0530

    update env variables

--- internal/modules/mint/query.py
+++ internal/modules/mint/query.py
@@ -1,19 +1,24 @@
-import os
-from utils import exec_command
+from utils import exec_command, env
 
-DAEMON = os.getenv('DAEMON')
-RPC = os.getenv('RPC')
-CHAINID = os.getenv('CHAINID')
+DAEMON = env.DAEMON
+RPC = env.RPC
+CHAINID = env.CHAINID
 
-# query_annual_provision fetches current minting annual provision value and returns response in json formate
+# query_annual_provision fetches current minting annual provision value 
+# and returns response in json formate
 def query_annual_provision():
     command = f"{DAEMON} q mint annual-provisions --node {RPC} --chain-id {CHAINID} --output json"
     return exec_command(command)
 
-# query_inflation fetches current minting inflation value and returns response in json formate
+
+# query_inflation fetches current minting inflation value and 
+# returns response in json formate
 def query_inflation():
-    command = f"{DAEMON} q mint inflation --node {RPC} --chain-id {CHAINID} --output json"
+    command = (
+        f"{DAEMON} q mint inflation --node {RPC} --chain-id {CHAINID} --output json"
+    )
     return exec_command(command)
+
 
 # query_params fetches mint module params
 def query_params():
