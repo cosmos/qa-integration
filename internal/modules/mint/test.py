71464aa962e382fab703b899f5b62764db7ae910
NagaTulasi <tulasitulip@gmail.com>
2022-07-25 16:58:09 +0530

    remove tests discription

--- internal/modules/mint/test.py
+++ internal/modules/mint/test.py
@@ -1,6 +1,3 @@
-"""
-This module contains all mint test functions.
-"""
 import logging
 import time
 import json
@@ -20,16 +17,7 @@ logging.info("INFO :: Running mint module test scripts")
 
 
 class TestMintModuleQueries(unittest.TestCase):
-    """
-    The class `TestMintModuleQueries` contains test functions
-    which test queries of mint module
-    """
-
     def test_params_query(self):
-        """
-        The function `test_params_account` will query
-        mint module params and test with genesis params
-        """
         # query params
         path = f"{HOME}/.simd-1/config/"
         with open(path + "genesis.json",encoding="utf8") as file:
@@ -41,19 +29,12 @@ class TestMintModuleQueries(unittest.TestCase):
         time.sleep(3)
 
     def test_inflation_query(self):
-        """
-        The function `test_inflation_query` will query inflation and test inflation 
-        """
         status, inflation = query_inflation()
         self.assertTrue(status)
         self.assertGreaterEqual(inflation, 0, "Querying minting inflation was failed")
         time.sleep(3)
 
     def test_annual_provision_query(self):
-        """
-        The function `test_annual_provision_query` will query annual provision
-        and test provision 
-        """
         status, provision = query_annual_provision()
         self.assertTrue(status)
         self.assertIsNotNone(provision, "Querying minting annual provision was failed")
