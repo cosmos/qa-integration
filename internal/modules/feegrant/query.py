faf37ce9b956c06dc20dbbe39c8de7ed7b57f129
Prathyusha Lakkireddy <prathyusha@vitwit.com>
2022-07-25 18:04:50 +0530

    Add feegrant module integration tests  (#40)
    * add grant tx
    
    * update grant tests
    
    * add revoke tx
    
    * add periodic grant tx
    
    * update feegrant tests
    
    * update periodic grant tx tests
    
    * fix typo
    
    * update tests
    
    * add assertions
    
    * add new line
    
    * update tests
    
    * update comments
    
    * add setupclass
    
    * upfdate tests
    
    * remove feegrant shell script file
    
    * address review comments
    
    * address review comments

---
+++ internal/modules/feegrant/query.py
@@ -0,0 +1,16 @@
+from utils import exec_command, env
+
+DAEMON = env.DAEMON
+RPC = env.RPC
+CHAINID = env.CHAINID
+
+# `query_grant` queries details of a single grant.
+def query_grant(granter, grantee):
+    command = f"{DAEMON} q feegrant grant {granter} {grantee} --node {RPC} --chain-id {CHAINID} --output json"
+    return exec_command(command)
+
+
+# `query_greantee_grants` queries all grants of a grantee.
+def query_greantee_grants(grantee):
+    command = f"{DAEMON} q feegrant grants {grantee} --node {RPC} --chain-id {CHAINID} --output json --count-total"
+    return exec_command(command)
