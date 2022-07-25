83e0a3e74a3bd22961a4bb99a15ea86f20f5cb79
NagaTulasi <tulasitulip@gmail.com>
2022-07-25 15:22:14 +0530

    fix lint issues

--- internal/modules/mint/test.py
+++ internal/modules/mint/test.py
@@ -1,5 +1,11 @@
-import logging, time, json, unittest
-from modules.mint.query import *
+"""
+This module contains all mint test functions.
+"""
+import logging
+import time
+import json
+import unittest
+from modules.mint.query import query_annual_provision, query_inflation, query_params
 from internal.utils import env
 
 HOME = env.HOME
@@ -10,7 +16,16 @@ logging.info("INFO :: Running mint module test scripts")
 
 
 class TestMintModuleQueries(unittest.TestCase):
+    """
+    The class `TestMintModuleQueries` contains test functions
+    which test queries of mint module
+    """
+
     def test_params_query(self):
+        """
+        The function `test_params_account` will query
+        mint module params and test with genesis params
+        """
         # query params
         path = f"{HOME}/.simd-1/config/"
         with open(path + "genesis.json") as file:
@@ -18,18 +33,23 @@ class TestMintModuleQueries(unittest.TestCase):
 
         query_param = query_params()[1]
 
-        assert (
-            data["app_state"]["mint"]["params"] == query_param
-        ), "missmatch in params"
+        assert data["app_state"]["mint"]["params"] == query_param, "missmatch in params"
         time.sleep(3)
 
     def test_inflation_query(self):
+        """
+        The function `test_inflation_query` will query inflation and test inflation 
+        """
         status, inflation = query_inflation()
         self.assertTrue(status)
         self.assertGreaterEqual(inflation, 0, "Querying minting inflation was failed")
         time.sleep(3)
 
     def test_annual_provision_query(self):
+        """
+        The function `test_annual_provision_query` will query annual provision
+        and test provision 
+        """
         status, provision = query_annual_provision()
         self.assertTrue(status)
         self.assertIsNotNone(provision, "Querying minting annual provision was failed")
