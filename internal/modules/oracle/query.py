290754bc20036c1e0f1810e56ad3781ded05858a
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2022-08-04 23:14:54 -0700

    add oracle module commands

---
+++ internal/modules/oracle/query.py
@@ -0,0 +1,43 @@
+from utils import exec_command, env
+
+DAEMON = env.DAEMON
+RPC = env.RPC
+CHAINID = env.CHAINID
+
+# query_exchange_rates queries the prices of all exchange rates
+def query_exchange_rates():
+    command = f"""{DAEMON} q oracle exchange-rates --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
+
+# query_exchange_rate queries the price of an exchange rate
+def query_exchange_rate(asset):
+    command = f"""{DAEMON} q oracle exchange-rate ${asset} --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
+
+# query_exchange_rate queries the price of an exchange rate
+def query_exchange_rate(asset):
+    command = f"""{DAEMON} q oracle exchange-rate ${asset} --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
+
+def query_aggregate_prevote(valAddress):
+    command = f"""{DAEMON} q oracle aggregate-prevotes ${valAddress} --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
+
+def query_aggregate_vote(valAddress):
+    command = f"""{DAEMON} q oracle aggregate-votes ${valAddress} --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
+
+def query_feeder_delegation(valAddress):
+    command = f"""{DAEMON} q oracle feeder-delegation ${valAddress} --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
+
+def query_miss_counter(valAddress):
+    command = f"""{DAEMON} q oracle miss-counter ${valAddress} --node {RPC} \
+--chain-id {CHAINID} --output json"""
+    return exec_command(command)
---
+++ internal/modules/oracle/tx.py
@@ -0,0 +1,34 @@
+from utils import exec_command, env
+
+DAEMON = env.DAEMON
+DAEMON_HOME = env.DAEMON_HOME
+RPC = env.RPC
+CHAINID = env.CHAINID
+DEFAULT_GAS = env.DEFAULT_GAS
+
+# tx_submit_prevote submits an aggregate prevote tx given a hash and
+# feeder address.
+def tx_submit_prevote(
+    from_key,
+    hash,
+    feeder_addr,
+    gas=DEFAULT_GAS,
+):
+    command = f"""{DAEMON} tx oracle exchange-rate-prevote {hash} {feeder_addr} \
+--chain-id {CHAINID} --keyring-backend test \
+--home {DAEMON_HOME}-1 --from {from_key} --node {RPC} --output json -y --gas {gas}"""
+    return exec_command(command)
+
+# tx_submit_vote submits an aggregate vote tx given a salt, exchange
+# rates, and a feeder address.
+def tx_submit_vote(
+    from_key,
+    salt,
+    exchange_rates,
+    feeder_addr,
+    gas=DEFAULT_GAS,
+):
+    command = f"""{DAEMON} tx oracle exchange-rate-vote {salt} {exchange_rates} {feeder_addr} \
+--chain-id {CHAINID} --keyring-backend test \
+--home {DAEMON_HOME}-1 --from {from_key} --node {RPC} --output json -y --gas {gas}"""
+    return exec_command(command)
